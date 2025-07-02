fc3f6d6a1772b2f580f4d739e7aae3a3efcb09f6
Eclipse <zoddd22@mail.ru>
2025-07-02 18:25:48 +0300

    Done

---
+++ main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"sync"
+)
+
+func main() {
+	code := make(chan int)
+	mode := make(chan int)
+	var wg sync.WaitGroup
+	wg.Add(1)
+	go func ()  {
+		arr(code)
+		wg.Done()
+	}()
+	wg.Add(1)
+	go func ()  {
+		cube(code, mode)
+		wg.Done()
+	}()
+	go func ()  {
+		wg.Wait()
+		close(code)
+		close(mode)
+	}()
+	for range 10 {
+		a := <- mode
+		fmt.Println(a) 
+	}
+}
+
+
+func arr(code chan int) {
+	arr := [10]int{}
+	for i := range arr {
+        arr[i] = rand.Intn(100)
+		code <- arr[i]
+    }
+}
+
+func cube(code, mode chan int) {
+	for range 10 {
+		x := <-code
+		mode <- x*x 
+	}
+}
